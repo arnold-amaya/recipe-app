0d9c40587739cd928c592bd7e25b2b6f180b453b
Arnold Amaya <arnold.amaya@01da.de>
2021-11-29 20:01:40 -0600

    add recipe detailed view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,3 +37,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'price', 'time_minutes', 'link', 'tags',
                   'ingredients')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
